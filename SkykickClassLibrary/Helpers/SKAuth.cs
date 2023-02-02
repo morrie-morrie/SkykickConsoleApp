dae5e89fd67db4cca707c8533c85788a182564c9
Andrew Morison <morrie@morrie.com>
2023-02-02 23:04:21 +1100

    - Further improvements to the Logic so that multiple auth and api calls aren't made needlessly.

--- SkykickClassLibrary/Helpers/SKAuth.cs
+++ SkykickClassLibrary/Helpers/SKAuth.cs
@@ -5,8 +5,10 @@ using SkykickClassLibrary.Models;
 
 namespace SkykickClassLibrary.Helpers;
 
+
 public class SKAuth
 {
+	static string? accessToken = null;
 	static string? bearer = null;
 	public static string CreateBearer()
 	{
@@ -22,21 +24,27 @@ public class SKAuth
 
 	public static string GetAccessToken()
 	{
-		var config = SKConfigHelper.ReadAppConfig();
-		var bearer = SKAuth.CreateBearer();
-		RestClient client = new RestSharp.RestClient("https://apis.skykick.com");
-		RestSharp.RestRequest request = new RestSharp.RestRequest("auth/token", RestSharp.Method.Post);
-		request.AddHeader("Authorization", bearer);
-		request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-		request.AddHeader("Ocp-Apim-Subscription-Key", $"{config.subId}");
-		request.AddParameter("grant_type=client_credentials&scope", "Partner");
-		RestSharp.RestResponse response = client.Execute(request);
-		var content = response.Content;
-		var token = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.TokenModel>(content);
+		if (accessToken == null)
+		{
+			var config = SKConfigHelper.ReadAppConfig();
+			var bearer = SKAuth.CreateBearer();
+			RestClient client = new RestSharp.RestClient("https://apis.skykick.com");
+			RestSharp.RestRequest request = new RestSharp.RestRequest("auth/token", RestSharp.Method.Post);
+			request.AddHeader("Authorization", bearer);
+			request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+			request.AddHeader("Ocp-Apim-Subscription-Key", $"{config.subId}");
+			request.AddParameter("grant_type=client_credentials&scope", "Partner");
+			RestSharp.RestResponse response = client.Execute(request);
+			var content = response.Content;
+			var token = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.TokenModel>(content);
 
-		return token.access_token;
+			accessToken = token.access_token;
+		}
+
+		return accessToken;
 	}
-	
+
+
 	public static List<Customer> GetCustomers()
 	{
 		var config = SKConfigHelper.ReadAppConfig();
--- SkykickClassLibrary/Helpers/SKConfigHelper.cs
+++ SkykickClassLibrary/Helpers/SKConfigHelper.cs
@@ -5,19 +5,24 @@ namespace SkykickClassLibrary.Helpers;
 
     public class SKConfigHelper
 {
+	private static ConfigModel? config = null;
+	
 	public static ConfigModel ReadAppConfig()
 	{
-		var config = new ConfigModel();
+		if (config == null)
+		{
+			config = new ConfigModel();
 
-		try
-		{
-			config.userId = ConfigurationManager.AppSettings["userId"];
-			config.subId = ConfigurationManager.AppSettings["SubID"];
-		}
-		catch (ConfigurationErrorsException)
-		{
-			Console.WriteLine("Error reading app settings");
-			throw;
+			try
+			{
+				config.userId = ConfigurationManager.AppSettings["userId"];
+				config.subId = ConfigurationManager.AppSettings["SubID"];
+			}
+			catch (ConfigurationErrorsException)
+			{
+				Console.WriteLine("Error reading app settings");
+				throw;
+			}
 		}
 		return config;
 	}
